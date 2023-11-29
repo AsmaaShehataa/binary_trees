30c56faf1f7770684a567117c230bd84fe908f07
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-11-29 23:51:01 +0200

    binary tree leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,7 +10,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
-else if (tree->left == NULL && tree->right != NULL)
+if (tree->left == NULL && tree->right == NULL)
 return (1); /* counting # of leaves */
 
 return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
