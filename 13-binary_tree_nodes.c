e36fdaec5361d3075fd6ace268e1cc542c98c639
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-11-30 16:27:35 +0200

    binary tree and 1 child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with 1 child
+ * @tree: a pointer to the root node of the tree
+ * Return: 0 if the tree is null
+*/
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+int countI = 0;
+if ((tree->left || tree->right) && tree)
+countI++;
+countI += (binary_tree_nodes(tree->left) +
+binary_tree_nodes(tree->right));
+
+return (countI);
+}
